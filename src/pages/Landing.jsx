19a7c375e47c543c5b8b8361171347bdda3421d1
Ridoan <ridoansalehnasution98@gmail.com>
2025-07-05 19:56:24 +0700

    feat: add initial landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { FaUsers, FaAngleDown } from "react-icons/fa";
 import NavBar from "../components/NavBar";
+import "../css/landing.css";
+import hotelRoom from "../assets/hotel-room.jpg";
 
 function Landing() {
+  const navigate = useNavigate();
+
   return (
-    <div className="page m-auto">
+    <div className="landing page m-auto">
       <NavBar />
-      <h3>Landing page</h3>
+      <img className="hero-image mt-24" src={hotelRoom} alt="Hero Image" />
+      <h2 className="title">Book a Room</h2>
+      <div className="search-bar">
+        <div className="input">
+          <div className="total-guest">
+            <FaUsers />
+            <span className="ml-5">2</span>
+          </div>
+          <FaAngleDown />
+        </div>
+        <div className="divider" />
+        <div className="input">
+          <span>Tue, 3 Jun 2025</span>
+          <FaAngleDown />
+        </div>
+      </div>
+      <div className="search-btn-wrapper">
+        <button className="search-btn" onClick={() => navigate("/book")}>
+          Search for Rooms
+        </button>
+      </div>
     </div>
   );
 }
