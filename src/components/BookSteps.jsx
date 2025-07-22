c0200ede4e8b860551abe39bf496c2c570046ebe
Ridoan <ridoansalehnasution98@gmail.com>
2025-07-22 12:16:25 +0700

    chore: hide the un-current steps in mobile

--- src/components/BookSteps.jsx
+++ src/components/BookSteps.jsx
@@ -4,24 +4,42 @@ function BookSteps({ step }) {
   return (
     <div className="book-steps">
       <div
-        className={`d-flex items-center mr-24 ${1 > step ? "not-active" : ""}`}
+        className={`
+          d-flex items-center mr-24 
+          ${1 > step ? "not-active" : ""}
+          ${1 !== step ? "not-current" : ""}
+        `}
       >
         <div className="circle mr-10">1</div>
         <label>Search</label>
       </div>
       <div
-        className={`d-flex items-center mr-24 ${2 > step ? "not-active" : ""}`}
+        className={`
+          d-flex items-center mr-24
+          ${2 > step ? "not-active" : ""}
+          ${2 !== step ? "not-current" : ""}
+        `}
       >
         <div className="circle mr-10">2</div>
         <label>Select Room</label>
       </div>
       <div
-        className={`d-flex items-center mr-24 ${3 > step ? "not-active" : ""}`}
+        className={`
+          d-flex items-center mr-24
+          ${3 > step ? "not-active" : ""}
+          ${3 !== step ? "not-current" : ""}
+        `}
       >
         <div className="circle mr-10">3</div>
         <label>Contact Information</label>
       </div>
-      <div className={`d-flex items-center ${4 > step ? "not-active" : ""}`}>
+      <div
+        className={`
+        d-flex items-center
+        ${4 > step ? "not-active" : ""}
+        ${4 !== step ? "not-current" : ""}
+      `}
+      >
         <div className="circle mr-10">4</div>
         <label>Confirmation</label>
       </div>
